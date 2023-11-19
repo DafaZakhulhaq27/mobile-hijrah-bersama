177a6657bd4fda35cc53260069f46d5bebe7d991
Dafa Zakhulhaq <dafa27890@gmail.com>
2023-11-19 08:49:41 +0700

    change base api url

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,4 +1,4 @@
-export const COLOR_PRIMARY = "#06b6d4"
-export const API_URL = "http://203.175.11.163:3900/mobile/user"
+export const COLOR_PRIMARY = "#06b6d4";
+export const API_URL = "http://203.175.11.163:3100/mobile/user";
 export const API_KEY =
-  '$2a$12$LnRdbXKAbhWaVbg6PWEbQ.H2wlcI0fHMcmidb628SCuwe5cNX8UHW'
+  "$2a$12$LnRdbXKAbhWaVbg6PWEbQ.H2wlcI0fHMcmidb628SCuwe5cNX8UHW";
